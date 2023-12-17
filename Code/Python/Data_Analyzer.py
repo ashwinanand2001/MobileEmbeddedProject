62bae63e9197c0ee7b9b183027ba667ff37e8d1e
nadnaks <59517789+nadnaks@users.noreply.github.com>
2023-12-16 19:14:24 -0500

    Add files via upload

--- Code/Python/Data_Analyzer.py
+++ Code/Python/Data_Analyzer.py
@@ -1,5 +1,5 @@
-
 #This file will read the signature data from the csv file and perform calculations to
+from re import X
 from tkinter import Y
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,15 +12,100 @@ from keras.models import Sequential
 from keras.layers import Conv1D, MaxPooling1D, Flatten, Dense
 import glob
 import os
+import csv
+import math
+
+def read_sensor_data(signer = ''):
+    #reading the files in directory
+    inp_data = os.listdir(path="/Users/skan/Documents/VSCode/MobileEmbeddedProject/SignatureData")
+
+    for val in inp_data:
+        if ".csv" not in val:
+            inp_data.remove(val)
+        if signer != '' and signer not in val:
+            inp_data.remove(val)
+
+    #extracting data from the files
+    sensor_data = []
+    signers = []
+
+    for file in inp_data:
+        df = pd.read_csv(os.path.join("/Users/skan/Documents/VSCode/MobileEmbeddedProject/SignatureData/", file))
+        df.apply(pd.to_numeric)
+        df = trim_sensor_data(df)
+        sensor_data.append(df)
+        signers.append(file.split(".")[0].rstrip('0123456789'))
+
+    create_sig_meta_file(sensor_data, signers)
+    return [sensor_data, signers]
+
+def create_sig_meta_file(sensor_data, signers):
+    #signers_flat = pd.Series(signers).drop_duplicates().tolist()
+
+    temp_dict = {}
+    for i in range(len(signers)):
+        #print(signers[i])
+        if signers[i] not in temp_dict:
+            temp_dict[signers[i]] = [sensor_data[i]]
+        else:
+            temp_dict[signers[i]].append(sensor_data[i])
 
 
-def predictSignature():
+    with open("signer_meta_data.csv", "w", newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['Name', 'Mean Signature Length', 'Avg STD Deviation Length'])
+        for signer in temp_dict:
+            [meanSigLength, stdDeviation] = standard_deviation_signature_length(temp_dict[signer], len(filter(bool, temp_dict[signer])))
+            csv_writer.writerow([signer, meanSigLength, stdDeviation])
+        
+
+def standard_deviation_signature_length(x_values, numValues):
+    meanSigLength = 0
+    stdDeviation = 0
+    if numValues > 1:
+        for value in x_values:
+            meanSigLength += value.shape[0]
+        meanSigLength /= len(x_values)
+        sumSquares = 0
+        for value in x_values:
+            sumSquares += pow(value.shape[0] - meanSigLength, 2)
+        stdDeviation = sumSquares/(len(x_values)-1)
+        stdDeviation = math.sqrt(stdDeviation)
+    else:
+        meanSigLength += x_values.shape[0]
+        stdDeviation = 0
+
+    return [meanSigLength, stdDeviation]
+
+def predictSignature(file):
+    
+    inp_data = os.path.join("/Users/skan/Documents/VSCode/MobileEmbeddedProject/SignatureData/",file)
+
+    # if ".csv" not in inp_data:
+    #     inp_data.remove(val)
+
+    #extracting data from the files
+    df = pd.read_csv(inp_data)
+    df.apply(pd.to_numeric)
+    df = trim_sensor_data(df)
+    signer = (file.split(".")[0].rstrip('0123456789'))
+    [meanSigLength, stdDeviation] = standard_deviation_signature_length(df, 1)
+
+    meta_data = pd.read_csv("signer_meta_data.csv")
+    meta_data.set_index('Name', inplace=True)
+    #print(meta_data)
+    #print(signer)
+    if meanSigLength > meta_data.loc[signer]["Mean Signature Length"] + 2*meta_data.loc[signer]["Avg STD Deviation Length"] \
+        or meanSigLength < meta_data.loc[signer]["Mean Signature Length"] - 2*meta_data.loc[signer]["Avg STD Deviation Length"]:
+        return "User Authentication Failed"
+    
+
+    return "User Authentication Success"
+
+#def evaluateModel():
 
 
-def evaluateModel():
-
-
-def trainModel(x_train, y_train):
+def train_model(x_train, y_train):
     sequence_length = x_train[0].shape[0]  # Adjust based on your data
     num_features = x_train[0].shape[1]
     x_train_reshaped = x_train.reshape((x_train.shape[0], sequence_length, num_features))
@@ -34,7 +119,7 @@ def trainModel(x_train, y_train):
     model.add(Dense(num_classes, activation='softmax'))
 
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit(X_train_reshaped, y_train, epochs=num_epochs, batch_size=batch_size, validation_split=0.2, verbose=2)
+    model.fit(x_train_reshaped, y_train, epochs=num_epochs, batch_size=batch_size, validation_split=0.2, verbose=2)
 
     return model
 
@@ -61,7 +146,7 @@ def extractWavelets(df):
 
 
 def find_start_end(roc):
-    print(len(roc))
+    #print(len(roc))
     for i in range(0,len(roc)):
         if roc[i] > .5:
             start = i
@@ -71,6 +156,14 @@ def find_start_end(roc):
             end = i
             break
     return [start, end]
+
+def trim_sensor_data(df):
+    d_total = average_roc(df)
+    [start,end] = find_start_end(d_total)
+    df = df.iloc[start:end]
+    print(end-start)
+    df = smoothData(df)
+    return df
 
 def average_roc(df):
     d_xAccel = np.gradient(df['xAccel'],df.index)
@@ -89,34 +182,20 @@ def average_roc(df):
     # plt.show()
     return d_total
 
-#reading the files in directory
-inp_data = os.listdir(path=r"")
-for val in inp_data:
-    if ".csv" not in val:
-        inp_data.remove(val)
+#[sensor_data, signers] = read_sensor_data()
 
-#extracting data from the files
-sensor_data = []
-signers = []
-for file in inp_data:
-    df = pd.read_csv(file)
-    df.apply(pd.to_numeric)
-    d_total = average_roc(df)
-    [start, end] = find_start_end(d_total)
-    df = df.iloc[start:end]
-    sensor_data.append(df)
-    signers.append(file.split("."))
+print(predictSignature("Skandan11.csv"))
 
 #split input data into training and test data - 50% split
-splitPercent = .5
-index = splitPercent*len(sensor_data) #same for x_traina and y_train
-x_train = sensor_data[0:index]
-y_train = signers[0:index]
-x_test = sensor_data[index+1:len(sensor_data)-1]
-y_test = signers[index+1:len(sensor_data)-1]
+# splitPercent = .5
+# index = splitPercent*len(sensor_data) #same for x_traina and y_train
+# x_train = sensor_data[0:index]
+# y_train = signers[0:index]
+# x_test = sensor_data[index+1:len(sensor_data)-1]
+# y_test = signers[index+1:len(sensor_data)-1]
 
 #train model
-trainModel()
+#train_model()
 
 
 # df = pd.read_csv("Skandan1.csv")
